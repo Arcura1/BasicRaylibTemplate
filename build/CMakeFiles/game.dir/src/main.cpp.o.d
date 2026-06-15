af0d6def0f4f75f8e8aedce2df4bf0397280a441
oisik <oisik@havelsan.com.tr>
2026-06-15 15:53:35 +0300

    init

---
+++ build/CMakeFiles/game.dir/src/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/game.dir/src/main.cpp.o: \
+ /home/kovan/Desktop/oray/raygame/src/main.cpp /usr/include/stdc-predef.h \
+ /home/kovan/Desktop/oray/raygame/external/raylib/src/raylib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
