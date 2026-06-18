59f64644a7fde42759bdaeacbbf860a902cf213d
oisik <oisik@havelsan.com.tr>
2026-06-18 15:09:54 +0300

    cleaning

--- build/CMakeFiles/game.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/game.dir/src/main.cpp.o: \
- /home/kovan/Desktop/oray/raygame/src/main.cpp /usr/include/stdc-predef.h \
- /home/kovan/Desktop/oray/raygame/external/raylib/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
--- build/external/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kovan/Desktop/oray/raygame/external/raylib
-# Build directory: /home/kovan/Desktop/oray/raygame/build/external/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
