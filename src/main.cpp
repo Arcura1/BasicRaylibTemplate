f9cb14725128b8578a1479e4e89a4ba985c04b25
oisik <oisik@havelsan.com.tr>
2026-06-29 18:34:41 +0300

    more bounce

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,64 @@ int main()
 
     SetTargetFPS(60);
     int size=10;
-    int x=1;
-    int y=1;
-    int x_direction=1;
-    int y_direction=1;
+    int x=10;
+    int y=100;
+    int x_speed=50;
+    int y_speed=50;
+    int gravitiy=1;
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
         DrawCircle(y,x,size,RED);
+
+        y=y+y_speed;
+        x=x+x_speed;
+
+        x_speed=x_speed+gravitiy;
         
+        if(x>=440)
+        {   
+            x=440;
+            x_speed=x_speed/2*-1;
+
+        }
+        if (x<=0)
+        {
+            x=0;
+            x_speed=x_speed/2*-1;
+        }
+
+        if(y>=800)
+        {
+            y=800;
+            y_speed=y_speed/2*-1;
+        }
+        if (y<=0)
+        {
+            y=0;
+            y_speed=y_speed/2*-1;
+        }
+
+
+
+  
+        DrawText("Hello Raylib!", 200, 150, 30, BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
+
+
+
+
+      /*
+
         if(x>=450)
         x_direction=-1;
         if (x<=0)
@@ -30,13 +77,4 @@ int main()
         x=x+x_direction;
 
         y=y+y_direction;
-
-        DrawText("Hello Raylib!", 200, 150, 30, BLACK);
-
-        EndDrawing();
-    }
-
-    CloseWindow();
-
-    return 0;
-}
+*/
