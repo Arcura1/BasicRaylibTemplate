ce894b31145ea425d02205da9ada4020022c76c1
oisik <oisik@havelsan.com.tr>
2026-06-29 17:56:22 +0300

    bounce

---
+++ build/CMakeFiles/game.dir/src/main.cpp.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/game.dir/src/main.cpp.o: \
+ /home/kovan/Desktop/oray/BasicRaylibTemplate/src/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/kovan/Desktop/oray/BasicRaylibTemplate/external/raylib/src/raylib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,33 @@ int main()
     InitWindow(800, 450, "Raylib Template");
 
     SetTargetFPS(60);
-
+    int size=10;
+    int x=1;
+    int y=1;
+    int x_direction=1;
+    int y_direction=1;
     while (!WindowShouldClose())
     {
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
+        DrawCircle(y,x,size,RED);
+        
+        if(x>=450)
+        x_direction=-1;
+        if (x<=0)
+        x_direction=1;
 
-        DrawText("Hello Raylib!", 250, 200, 30, BLACK);
+        if(y>=800)
+        y_direction=-1;
+        if (y<=0)
+        y_direction=1;
+        
+        x=x+x_direction;
+
+        y=y+y_direction;
+
+        DrawText("Hello Raylib!", 200, 150, 30, BLACK);
 
         EndDrawing();
     }
